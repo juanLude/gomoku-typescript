87c2273136b67ffc1227de82a157b12bcb60e495
Juan Ludevid <jmludevid@hotmail.com>
2023-07-15 00:54:55 +1000

    generate game board

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
-console.log("hello");
+console.log("hola");
+var Board = /** @class */ (function () {
+    function Board(rows, columns) {
+        // initialise the board and currentPlayer properties
+        this.board = [];
+        this.currentPlayer = "Black"; // set first turn to Black
+        for (var row = 0; row < rows; row++) {
+            // initialise each box of the board with a null value
+            this.board[row] = [];
+            for (var col = 0; col < columns; col++) {
+                this.board[row][col] = null;
+            }
+        }
+    }
+    return Board;
+}());
+var MarkupGenerator = /** @class */ (function () {
+    function MarkupGenerator() {
+    }
+    MarkupGenerator.prototype.createMarkup = function (numRows, numBoxesPerRow) {
+        var container = document.createElement("div");
+        for (var i = 0; i < numRows; i++) {
+            var row = document.createElement("div");
+            row.classList.add("row");
+            for (var j = 0; j < numBoxesPerRow; j++) {
+                var box = document.createElement("div");
+                box.classList.add("box");
+                row.appendChild(box);
+            }
+            container.appendChild(row);
+        }
+        return container;
+    };
+    return MarkupGenerator;
+}());
+// Usage:
+var generator = new MarkupGenerator();
+var numRows = 2;
+var numBoxesPerRow = 4;
+var markup = generator.createMarkup(numRows, numBoxesPerRow);
+console.log(markup);
+// Append the markup to a target element with an ID
+var targetElement = document.getElementById("target");
+if (targetElement) {
+    targetElement.appendChild(markup);
+}
--- app.ts
+++ app.ts
@@ -1,1 +1,55 @@
-console.log("hello");
+console.log("hola");
+
+// This type represents the possible values that a player can have
+type Player = "Black" | "White" | null;
+
+class Board {
+  private board: Player[][]; // This 2D array represents the game board and maps the current state of each box
+  private currentPlayer: Player; // Defines the player's turn
+
+  constructor(rows: number, columns: number) {
+    // initialise the board and currentPlayer properties
+    this.board = [];
+    this.currentPlayer = "Black"; // set first turn to Black
+    for (let row = 0; row < rows; row++) {
+      // initialise each box of the board with a null value
+      this.board[row] = [];
+      for (let col = 0; col < columns; col++) {
+        this.board[row][col] = null;
+      }
+    }
+  }
+}
+
+class MarkupGenerator {
+  createMarkup(numRows: number, numBoxesPerRow: number): HTMLElement {
+    const container = document.createElement("div");
+
+    for (let i = 0; i < numRows; i++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+
+      for (let j = 0; j < numBoxesPerRow; j++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        row.appendChild(box);
+      }
+
+      container.appendChild(row);
+    }
+
+    return container;
+  }
+}
+
+const generator = new MarkupGenerator();
+const numRows = 2;
+const numBoxesPerRow = 4;
+const markup = generator.createMarkup(numRows, numBoxesPerRow);
+console.log(markup);
+
+// Append the markup to a target element with an ID
+const targetElement = document.getElementById("target");
+if (targetElement) {
+  targetElement.appendChild(markup);
+}
